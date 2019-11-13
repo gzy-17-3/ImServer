f63824b23fc153637f0413d2745df56ce1931a1d
lxf <rdxer@foxmail.com>
2019-11-13 14:49:37 +0800

    。

--- src/main/java/com/gzy/im/service/AccountService.java
+++ src/main/java/com/gzy/im/service/AccountService.java
@@ -28,6 +28,7 @@ public class AccountService {
 
     public Account updateAccount(Long uid, Account accountPara) {
         Optional<Account> byId = accountRepository.findById(uid);
+
         if (byId.isEmpty()){
             throw new NotFoundException();
         }
