d775ed499206f10d2671e21880bb5845ba700abf
lxf <rdxer@foxmail.com>
2019-11-13 14:48:38 +0800

    自定义异常

--- src/main/java/com/gzy/im/controller/MineController.java
+++ src/main/java/com/gzy/im/controller/MineController.java
@@ -1,5 +1,7 @@
 package com.gzy.im.controller;
 
+import com.gzy.im.core.exception.ForbiddenException;
+import com.gzy.im.core.exception.NotFoundException;
 import com.gzy.im.model.Account;
 import com.gzy.im.service.AccountService;
 import com.gzy.im.service.AppUserDetailsService;
@@ -10,6 +12,19 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 
+/*
+
+ 异常
+ 2
+ 1. 运行时异常 RuntimeException
+    - 不需要捕获
+ 2. 普通异常 Exception
+    - 需要进行处理
+
+ */
+
+
+
 @RestController
 @RequestMapping("mine/")
 public class MineController {
@@ -17,13 +32,14 @@ public class MineController {
     AccountService accountService;
 //    获取当前用户信息
     @GetMapping("/{uid}")
-    Object account(@PathVariable Long uid, @AuthenticationPrincipal AppUserDetailsService.AppUserDetails appUserDetails){
+    Account account(@PathVariable Long uid, @AuthenticationPrincipal AppUserDetailsService.AppUserDetails appUserDetails)  {
         System.out.println(appUserDetails);
+
 
         // 判断是不是当前用户
 
         if (!uid.equals(appUserDetails.account.getId())){
-            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+            throw new ForbiddenException();
         }
 
         return accountService.getAccount(uid);
@@ -31,12 +47,12 @@ public class MineController {
 
 //    修改信息
     @PutMapping("/{uid}")
-    Object updateAccount(@PathVariable Long uid,
+    Account updateAccount(@PathVariable Long uid,
                          @AuthenticationPrincipal AppUserDetailsService.AppUserDetails appUserDetails,
                          @RequestBody Account account
     ){
         if (!uid.equals(appUserDetails.account.getId())){
-            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+            throw new ForbiddenException();
         }
 
         return accountService.updateAccount(uid,account);
--- src/main/java/com/gzy/im/service/AccountService.java
+++ src/main/java/com/gzy/im/service/AccountService.java
@@ -1,10 +1,12 @@
 package com.gzy.im.service;
 
 import com.gzy.im.core.AppBeanUtils;
+import com.gzy.im.core.exception.NotFoundException;
 import com.gzy.im.model.Account;
 import com.gzy.im.repository.AccountRepository;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
 
 import javax.annotation.Resource;
 import java.util.Optional;
@@ -17,12 +19,19 @@ public class AccountService {
     public Account getAccount(Long uid) {
         Optional<Account> byId = accountRepository.findById(uid);
         // 如果为空  应该直接返回 404
+        if (byId.isEmpty()){
+            throw new NotFoundException();
+        }
 
         return byId.get();
     }
 
     public Account updateAccount(Long uid, Account accountPara) {
         Optional<Account> byId = accountRepository.findById(uid);
+        
+        if (byId.isEmpty()){
+            throw new NotFoundException();
+        }
 
         Account account1 = byId.get();
 
